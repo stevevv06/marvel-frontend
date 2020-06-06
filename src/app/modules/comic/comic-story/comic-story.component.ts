e2cbbe295ff4d4f83c787438c9c41518f31729b3
jaguarsoftsv <jaguarsoftsv@gmail.com>
2020-06-06 12:53:59 -0600

    Adding comics characters  and stories components and navigation

--- src/app/modules/comic/comic-story/comic-story.component.ts
+++ src/app/modules/comic/comic-story/comic-story.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { Subscription } from 'rxjs';
+import { ComicService } from '../comic.service';
+import { LayoutService } from '../../shared/layout.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-comic-story',
@@ -7,9 +12,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComicStoryComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  item: any = {};
+  data: any[];
+  private subscription: Subscription;
+
+  totalItems: number; 
+  pageSize: number = 20;
+  pageSizeOptions: number[] = [20, 50, 100];
+  page: number;
+
+  constructor(
+    private comicService: ComicService,
+    public layoutService: LayoutService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.subscription = this.route.params
+      .subscribe((params) => {
+        this.id = params['id'];
+        this.get();
+    });
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+}
+
+  get() {
+    this.comicService.get(this.id).subscribe(
+      (res: any) => {
+        this.item = res.data.results[0];
+      });
+    this.comicService.getStories(this.id, {
+      limit: this.pageSize,
+      offset: this.pageSize * this.page
+    }).subscribe(
+      (res: any) => {
+        this.totalItems = res.data.total;
+        this.data = res.data.results;
+      });
+  }
+
+  paginate(event: PageEvent) {
+    this.page = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.get();
+  }
 }
